4ef126708389ea48545f8972d30a5f21cd396716
Yurii Smolii <ysmolii@roku.com>
2022-11-07 09:34:03 +0200

    Home work 11

--- main.js
+++ main.js
@@ -8,26 +8,28 @@ async function getUsers() {
     await fetch(USERS_URL)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-
-        data.forEach(item => {
-            let elementImg = document.createElement('img')
-            let elementLogin = document.createElement('h4')
-            let elementRepoUrl = document.createElement('p')
-            let elementArticle = document.createElement('article')
-
-            elementImg.setAttribute('src', item.avatar_url)
-            elementLogin.appendChild(document.createTextNode(item.login))
-            elementRepoUrl.appendChild(document.createTextNode(item.repos_url))
-
-            elementArticle.appendChild(elementImg)
-            elementArticle.appendChild(elementLogin)
-            elementArticle.appendChild(elementRepoUrl)
-
-            container.appendChild(elementArticle)
-        });   
+        renderUsers(data)
     });
 }
+function renderUsers(data){
+    data.forEach(item => {
+        let elementImg = document.createElement('img')
+        let elementLogin = document.createElement('h4')
+        let elementRepoUrl = document.createElement('p')
+        let elementArticle = document.createElement('div')
+
+        elementImg.setAttribute('src', item.avatar_url)
+        elementLogin.appendChild(document.createTextNode(item.login))
+        elementRepoUrl.appendChild(document.createTextNode(item.repos_url))
+
+        elementArticle.appendChild(elementImg)
+        elementArticle.appendChild(elementLogin)
+        elementArticle.appendChild(elementRepoUrl)
+
+        container.appendChild(elementArticle)
+    }); 
+}
+
 getUsers();
 
 
